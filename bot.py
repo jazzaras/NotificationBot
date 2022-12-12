b2011326a04d30fa5ee4d329c831e08b6d1d159f
jazzaras <jazzaras@yahoo.com>
2022-12-12 18:09:00 +0300

    done is a list... removed some errors... every thing look ok

--- bot.py
+++ bot.py
@@ -29,6 +29,30 @@ def stringToList(StringinListFormat):
 	theList = [item.strip() for item in theList]
 	return theList
 
+def clear(Chatid):
+
+	df = pandas.read_excel(filename, sheet_name='main')
+	wb = load_workbook(filename)
+	ws = wb.worksheets[0]
+
+	exelIndex = (FindingIDIndex(Chatid) + 2)
+
+	posDAY = 'B' + str(exelIndex)
+	posTIME = 'C' + str(exelIndex)
+	posDONE = 'D' + str(exelIndex)
+	posSTATE = 'E' + str(exelIndex)
+	posHOLD = 'F' + str(exelIndex)	
+
+
+
+	ws[posTIME] = "[]"
+	ws[posDAY] = "[]"
+	ws[posDONE] = "[]"
+	ws[posSTATE] = "non"
+	ws[posHOLD] = "nothing"
+
+	wb.save(filename)
+
 # a function that loops through our excel file and send messages if time, day is now
 def checkingNotification():
 	
@@ -61,20 +85,21 @@ def checkingNotification():
 		for i in range(len(ids)):
 
 			userdays = stringToList(day[i])
-
 			usertimes = stringToList(time[i])
+			userdones = stringToList(done[i]) 
 
 			for x in range(len(userdays)):
 				if dayName == userdays[x]:
 					if currentTime == usertimes[x]:
-						if not done[i]:
+						if userdones[x] == "False":
 							# Send Noti0fication
 
 							bot.send_message(ids[i], "you have a class now")
 
-							# Set Done to True --> so it will not go in the if statment for a whole minute
+							# Set Done (one item in the list) to True --> so it will not go in the if statment for a whole minute
 							cell = "D" + str(i+2)
-							ws[cell] = True
+							userdones[x] = "True"
+							ws[cell] = str(userdones)
 							wb.save(filename)
 
 							print("sent")
@@ -84,14 +109,20 @@ def checkingNotification():
 		# Reset ALL Done boolean variables in excel				
 		
 		if currentTime == "00:00":
-
+			
 			for i in range(len(ids)):
+				userdones = stringToList(done[i])
+				print(userdones)
+				cell = "D" + str(i+2)								 
+				for x in range(userdones[i]):
 
-				cell = "D" + str(i+2)
+					userdones[x] = "False"
 
-				ws[cell] = False
+				ws[cell] = str(userdones)
+				wb.save(filename)
 
-				wb.save(filename)			
+			print("cleared all dones")
+
 
 ### making a thread so that the bot thread will not be disturbed by our checking loop
 check = threading.Thread(target=checkingNotification)
@@ -128,11 +159,14 @@ def FindingIDIndex(Chatid):
         
         id_ = str(ids[i])
 
+		# print(id_, Chatid)
 
-        if id_ == Chatid:
+        if id_ == str(Chatid):
             cell = i
             return (cell)
     return (cell)    
+
+
 
 def addNotification(Chatid, Day ,Time):
 
@@ -153,6 +187,9 @@ def addNotification(Chatid, Day ,Time):
 	days = df['Day'].tolist()
 	userDays = stringToList(days[IDI]) # type = str
 
+	dones = df['Done'].tolist()
+	userDones = stringToList(dones[IDI])
+
 	# userDays = json.loads(userDays) # type = list  
 
 	exelIndex = IDI + 2
@@ -168,6 +205,7 @@ def addNotification(Chatid, Day ,Time):
 
 	posDAY = 'B' + str(exelIndex)
 	posTIME = 'C' + str(exelIndex)
+	posDONE = 'D' + str(exelIndex)
 	posSTATE = 'E' + str(exelIndex)
 	posHOLD = 'F' + str(exelIndex)
 	
@@ -177,8 +215,13 @@ def addNotification(Chatid, Day ,Time):
 	userDays.append(Day)
 	userDays = str(userDays)
 
+	userDones.append('False')
+	userDones = str(userDones)
+
+
 	ws[posDAY] = userDays
 	ws[posTIME] = userTimes
+	ws[posDONE] = userDones
 	ws[posSTATE] = "sce"	
 	ws[posHOLD] = "nothing"
 
@@ -193,7 +236,8 @@ def addNotification(Chatid, Day ,Time):
 
 @bot.message_handler(commands=["start", "help"])
 def welcome(message):
-	bot.send_message(message.chat.id, "welcome to the graetest bot ever made")
+	bot.send_message(message.chat.id, f"hay {message.from_user.first_name} \n welcome to the graetest bot ever made")
+	bot.send_message(message.chat.id, "commamds: \n /add ==> to add weekly notifications \n /clear ==> to clear all notifictions")
 
 	df = pandas.read_excel(filename, sheet_name='main')
 	wb = load_workbook(filename)
@@ -215,7 +259,7 @@ def welcome(message):
 
 
 	ws[posID] = message.chat.id
-	ws[posDONE] = False
+	ws[posDONE] = "[]"
 	ws[posDAY] = "[]"
 	ws[posTIME] = "[]"	
 	ws[posSTATE] = "non"
@@ -240,6 +284,13 @@ def adding(message):
 	ws[posSTATE] = 'waitingForDay'
 	wb.save(filename)
 
+@bot.message_handler(commands=["clear", "Clear"])
+def clearing(message):
+
+	clear(message.chat.id)
+
+	bot.send_message(message.chat.id, "Done... All notifications where deleted")
+
 
 	
 @bot.message_handler()
@@ -257,23 +308,30 @@ def general(message):
 	posSTATE = 'E' + str(exelIndex)
 	posHOLD = 'F' + str(exelIndex)
 
-
 	states = df["State"].tolist()
+
+	# print(states, IDI)
+
 	userStat = states[IDI]
 
 	holdValues = df["Hold"].tolist()
 	userHoldValue = holdValues[IDI]	
 
+	# print(userStat)
 
 	if (userStat == "non") or (userStat == "sce"):
-		bot.reply_to(message, "Enter a command")
+		if message.text == "hay" or message.text == "Hay":
+			print(message)
+			bot.reply_to(message, "Hello")
+		else:
+			bot.reply_to(message, "I did not understand...\n you can ask for /help if your lost \n or you can say hay...")
 	
 	elif userStat == "waitingForDay":
 		if mes in days:
 			ws[posHOLD] = mes
 			ws[posSTATE] = 'waitingForTime'
 			wb.save(filename)
-			bot.reply_to(message, "good, now enter a time ex.(14:59,, 09:12, 15:02)")
+			bot.reply_to(message, "good, now enter a time in 24H Format ex.(14:59,, 09:12, 15:02)")
 		else:
 			bot.reply_to(message, "not a valid Day")
 	
@@ -293,13 +351,18 @@ def general(message):
 			bot.reply_to(message, "Perfect... your notification has been added")
 			
 		else:
-			bot.reply_to(message, "not a valid Time")
-			bot.send_message(message.chat.id, "enter a time ex.(14:59,, 09:12, 15:02)")
+			bot.reply_to(message, "not a valid Time \nmake sure you enter Time with zeros.. ex: \n 01:20")
 
 
 
 
 check.start()
 
-bot.polling()
+while True:
+    try:
+        bot.polling(non_stop=True, interval=0)
+    except Exception as e:
+        print("error: ", e)
+        thetime.sleep(5)
+        continue
 
